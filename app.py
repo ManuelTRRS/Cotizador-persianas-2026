aff4ec7f337a60eb7d87f77122702d2050c9b85a
Manuel Torres <16327845.torres@ms.uas.edu.mx>
2026-01-29 08:37:42 -0700

    Calculadora de precios funcionando

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def inicio():
-    return "Servidor activo"
+    resultado=None
+    if request.method == 'POST':
+        Ancho=float(request.form['Ancho'])
+        Alto=float(request.form['Alto'])
+        m2=500
+        total=Ancho*Alto*m2
+        resultado=total
+        pass
+    return render_template('index.html', resultado_cotizacion=resultado)
 
 if (__name__ == '__main__'):
     app.run(debug=True)
